76eaf21ca6c5e452341a0308edeb7ce872c0354c
kevin <kevinhellman94@gmail.com>
2021-03-04 19:40:47 -0500

    Updated Controller to include post and put routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,4 +14,29 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('api/burgers', (req, res) => {
+    burger.insertOne(['burger_name','devoured'], [req.body.name, req.body.devoured], (result) => {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put('/api/cats/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`
+
+    console.log('condition', condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured,
+        },
+        condition,
+        (results) => {
+            if (results.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
 module.exports = router;
