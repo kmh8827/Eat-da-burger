066d0b7d9121a6bd2b301ea64c925d8fa83a67e0
kevin <kevinhellman94@gmail.com>
2021-03-04 19:35:52 -0500

    Updated Partial

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,17 @@
 const express = require('express');
+
+const router = express.Router();
+
 const burger = require('../models/burger');
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        const hbsObject = {
+            burgers: data,
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
 
 module.exports = router;
