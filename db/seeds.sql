0149b4f333a37cc4b4a115c7276854b1c7af4619
kevin <kevinhellman94@gmail.com>
2021-03-04 19:17:26 -0500

    Fixed sql seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-insert into burgers (burger_name, devoured) values (hamburger, true);
-insert into burgers (burger_name, devoured) values (cheesburger, true);
-insert into burgers (burger_name, devoured) values (doublehamburger, false);
-insert into burgers (burger_name, devoured) values (doublecheeseburger, false);
+insert into burgers (burger_name, devoured) values ('hamburger', true);
+insert into burgers (burger_name, devoured) values ('cheesburger', true);
+insert into burgers (burger_name, devoured) values ('doublehamburger', false);
+insert into burgers (burger_name, devoured) values ('doublecheeseburger', false);
